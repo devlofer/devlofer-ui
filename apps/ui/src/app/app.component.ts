3b787c2550467f8912df2455e7d04cd460bd7866
2RajShaikh <raj.shaikh@kaplan.com>
2020-12-24 16:11:32 -0500

    initial commit

---
+++ apps/ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'devlofer-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ui';
+}
